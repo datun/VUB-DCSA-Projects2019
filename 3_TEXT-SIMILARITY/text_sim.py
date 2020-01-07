c0be0f4e780c74457e4de04f16a7c3e5a4d2254f
dAtun <denizatun@gmail.com>
2020-01-07 15:00:08 +0100

    Slowly migrating main.py to text_sim.py

---
+++ 3_TEXT-SIMILARITY/text_sim.py
@@ -0,0 +1,72 @@
+import numpy as np
+import json, random, re
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+def process_str2list(in_1):
+    pass_1 = in_1.replace("\n", " ")
+    pass_2 = re.split(r"[\.|\,|\(|\)|\s]+", pass_1)
+    return pass_2[:-1]
+
+def randompick(json_in):
+    rng = random.randrange(0, len(json_in))
+    return json_in[rng]['summary'], rng
+
+def JacardCoef_2(t_a,t_b):
+    # According to wikipedia intersection description, calculation by list-similarity.
+    t_a = process_str2list(t_a)
+    t_b = process_str2list(t_b)
+    dividend = np.intersect1d(t_a, t_b)
+    divisor = np.union1d(t_a, t_b)
+    sim = np.divide(len(dividend),len(divisor))
+    return sim
+
+
+class MR_Jaccard(MRJob):
+    art_sum = None
+    index = None
+    def steps(self):
+
+        #     Two ways of doing the same job!
+        #     Step 1: Load the file and parse summaries
+        #     Step 2: Pick a random summary to be compared one
+        #     Step 3: Run the chosen file through MR to find the word frequencies, hold it in memory.
+        #     Step 4: Start word frequencies of rest of the list and using given function find similarities and store it
+        #     Step 5: Output result
+        #
+        #     Better way or say the simpler one
+        #     Step 1: Load file and parse summaries only.
+        #     Step 2: Calculate word frequencies for all files.
+        #     Step 3: Save or output those results to be used in next part (transmission)
+        #     Step 4: Pick a random summary
+        #     Step 5: Run the function of random summary with rest of the summary
+        #     Step 6: Output result
+        return [
+                MRStep(mapper_raw=self.mapper_raw,),
+                MRStep(mapper=self.mapper,
+                       reducer=self.reducer,)
+               ]
+
+    def mapper_raw(self, input_path, input_uri):
+        with open(input_path) as file:
+            try:  # load JSON file given in that directory, if fails raise Exception
+                x = json.load(file)
+            except ValueError:
+                raise Exception('Input file is not python loadable JSON file')
+        art_sum, index = randompick(x)
+        yield(len(x), x)
+
+    def mapper(self, _, json_in):
+        for i in range(json_in):
+            if not i == self.index:
+                result = JacardCoef_2(self.art_sum, json_in[i]['summary'])
+            yield (i, result)
+
+    def combiner(self, word, counts):
+        yield (word, sum(counts))
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+if __name__ == '__main__':
+    MR_Jaccard.run()
