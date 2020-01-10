4573bd802f5a9a651685d7f3112dc3cc476e24ea
dAtun <denizatun@gmail.com>
2020-01-10 04:29:53 +0100

    Matrix Multiplication done

--- 4_MATRIX/matrix_mult.py
+++ 4_MATRIX/matrix_mult.py
@@ -1,36 +1,40 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-import os
 
-
-row_counter = -1
-column_counter = -1
+dim_Arows = -1
+dim_Acolumns = 0
+dim_Brows = -1
+dim_Bcolumns = 0
 
 
 class MR_MatrixMult(MRJob):
-
     def steps(self):
-        return [MRStep(mapper=self.map_mats,
-                       reducer=self.red_multip),
-                MRStep(mapper=self.map_result,
-                       reducer=self.red_sumresult)]
+        return [MRStep(mapper_raw=self.map_values,      # Mapping the matrix elements to be able to process better
+                       reducer=self.red_multip),        # Multiplying and tagging elements to sum in the next step
+                MRStep(mapper=self.map_result,          # Mapping the tags -> key
+                       reducer=self.red_sumresult)]     # Summing the elements with same key, then it is done!
 
-    def map_mats(self, _, line):
-        global row_counter
-        global column_counter
-        mat_in = line.split()
-        filename = os.environ['map_input_file']
+    def map_values(self,input_path, input_uri):
+        global dim_Arows
+        global dim_Acolumns
+        global dim_Brows
+        global dim_Bcolumns
+        x = open(input_path,"r")
 
-        # For matrix A (i,k)
-        if 'A.txt' in filename:
-            row_counter += 1
-            for k in range(len(mat_in)):
-                yield row_counter, ("A", k, mat_in[k])
-        # For matrix B (k,j)
-        if 'B.txt' in filename:
-            column_counter += 1
-            for j in range(len(mat_in)):
-                yield column_counter, ("B", j, mat_in[j])
+        for line in x:
+            row_in = line.split()
+            if 'A.txt' in input_path:
+                if dim_Acolumns == 0:
+                    dim_Acolumns = len(row_in) -1
+                dim_Arows += 1
+                for j in range(len(row_in)):
+                    yield j, ("A", dim_Arows,row_in[j])
+            if 'B.txt' in input_path:
+                if dim_Bcolumns == 0:
+                    dim_Bcolumns = len(row_in) -1
+                dim_Brows += 1
+                for j in range(len(row_in)):
+                    yield dim_Brows, ("B", j, row_in[j])
 
     def red_multip(self, key, val):
         listA = []
@@ -38,15 +42,15 @@ class MR_MatrixMult(MRJob):
         for temp in val:
             if temp[0] == "A":
                 listA.append(temp)
-            else:
+            elif temp[0] == "B":
                 listB.append(temp)
 
         for a in listA:
             for b in listB:
-                yield (a[1],b[1]), (float(a[2]) * float(b[2]))
+                yield (a[1], b[1]), float(a[2]) * float(b[2])
 
     def map_result(self, key, val):
-        yield (key, val)
+        yield key, val
 
     def red_sumresult(self, key, val):
         yield (key, sum(val))
