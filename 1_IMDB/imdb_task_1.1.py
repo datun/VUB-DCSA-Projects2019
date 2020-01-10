ab1c50ac9d50b83f0bc3f4beab4043548a4e30b4
dAtun <denizatun@gmail.com>
2020-01-10 23:13:40 +0100

    Many fixes and report
    Task 1.1 was half done, did rest of it and also was incorrect. Fixed that also.
    
    Began writing report, will be also checking Jaccard Implementation later

--- 1_IMDB/imdb_task_1.1.py
+++ 1_IMDB/imdb_task_1.1.py
@@ -1,6 +1,12 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 import re
 import nltk
+from nltk.corpus import stopwords
+stopwords = set(stopwords.words(["arabic", "azerbaijani", "danish", "dutch", "english", "finnish", "french", "german",
+                                 "greek", "hungarian", "indonesian", "italian", "kazakh", "nepali", "norwegian",
+                                 "portuguese", "romanian", "russian","slovene", "spanish", "swedish",
+                                 "tajik", "turkish"]))
 
 # Status:
 # Picking rows with correct values [X]
@@ -16,12 +22,20 @@ import nltk
 # Due to the multiple languages within titles and how MRJob handles those files, the output
 # files contain decoded unicode strings such as "Açúcar" => "A\u00e7\u00facar"
 
+counter = -1
+val_list = []
 
 class MRWordFreqCount(MRJob):
+    def steps(self):
+        return [MRStep(mapper=self.mapper,
+                       combiner=self.combiner,
+                       reducer=self.reducer),
+                MRStep(reducer=self.sorter)]
+
     def mapper(self, _, line):
         pre_process = re.split(r"[\t]", line)  # Splitting files according to tabs
         if pre_process[1] in ("movie", "short"):  # 2nd column shows the type
-            post_process = re.findall("[^\W\d]{2,}", pre_process[2])
+            post_process = re.findall("[^\W\d]{2,}", pre_process[2].lower())
             # Find non-whitespace and non-digits with more than 2 characters (to prevent 's 'l stuff)
             lang_process = " ".join(post_process)  # joining the results for token process
             tokens = nltk.word_tokenize(lang_process)  # 3rd column contains the original movie name
@@ -29,14 +43,27 @@ class MRWordFreqCount(MRJob):
             for tagged, tags in tagged:
                 # Only accepting words that are not
                 # IN=Prepositions, DT=Determiner, CONJ=Conjunctions, SYM=Symbols, AT=Articles
-                if tags not in ("IN", "AT", "CONJ", "SYM", "DT", "POS", "RB", "PRT", "NUM", "."):
-                    yield (tagged, 1)
+                if tags not in ('IN', 'AT', 'CONJ', 'SYM', 'DT', 'POS', 'RB', 'PRT', 'NUM', 'TO', 'CC', '.'):
+                    if tagged not in stopwords:
+                        yield (tagged, 1)
 
     def combiner(self, word, counts):
         yield (word, sum(counts))
 
     def reducer(self, word, counts):
-        yield (word, sum(counts))
+        yield None, (sum(counts), word)
+
+    def sorter(self, _, result):
+        global val_list
+        for counts, word in result:
+            if len(val_list) <= 50:
+                val_list.append((int(counts), word))
+            else:
+                if min(val_list)[0] < int(counts):
+                    val_list.remove(min(val_list))
+                    val_list.append((int(counts),word))
+        for i in sorted(val_list, reverse=True):
+            yield i
 
 
 if __name__ == '__main__':
